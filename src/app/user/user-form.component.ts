6c60da0492787c14cecf7ef1280060a21550b990
ravlu <ravi.kambhampati1@gmail.com>
2021-08-04 20:08:04 +0530

    validation status

--- src/app/user/user-form.component.ts
+++ src/app/user/user-form.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormControl, Validators } from '@angular/forms';
 @Component({
   selector: 'user-form',
   template:
-    '<label>Name</label><input id="name" [formControl]="name" /><p>{{name.value}}</p>'
+    '<label>Name</label><input id="name" [formControl]="name" /><p>{{ name.value }}</p>  <button type="submit" [disabled]="!name.valid">Submit</button><p>{{ name.valid }}</p><p>{{ name.status }}</p>'
 })
 export class UserFormComponent {
-  name = new FormControl('');
+  name = new FormControl('', Validators.required);
 }
